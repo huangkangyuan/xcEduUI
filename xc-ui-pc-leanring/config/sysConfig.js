d942c06b81ad0a51f3f769c67f7052dc313477a4
root <614772276@qq.com>
2019-06-09 13:14:35 +0800

    学习中心

---
+++ xc-ui-pc-leanring/config/sysConfig.js
@@ -0,0 +1,12 @@
+var sysConfig = {
+    xcApiUrlPre: '/api',//客户端请求
+    staticURL: '/static',
+    backApiURL: 'http://www.xc.com/openapi',//服务端请求
+    backStaticURL: 'http://www.xc.com/static',
+    imgUrl:'http://img.xc.com/',
+    videoUrl:'http://video.xc.com/video/',
+    openAuthenticate:true,
+    openAuthorize:true
+}
+
+module.exports = sysConfig
